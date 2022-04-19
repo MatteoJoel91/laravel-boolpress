49df5464a71f31443f3a5af61eb6d059f551254b
Matteo Gioele Cocomazzi <TheZefyro@gmail.com>
2022-04-19 17:50:06 +0200

    push esercizio 19/04/2022

--- resources/js/pages/Contact.vue
+++ resources/js/pages/Contact.vue
@@ -1,14 +1,89 @@
 <template>
   <div class="container">
-      <h1 class="py-3 text-center">Per contattarci usa i nostri recapiti</h1>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti quidem iste quibusdam doloremque saepe expedita facilis temporibus error, tenetur voluptates doloribus pariatur voluptatum ipsam? Consequatur nemo sit in excepturi numquam!</p>
+
+    <div class="row">
+      <div class="col-12">
+        <form @submit.prevent="sendForm">
+
+          <div v-if="success" class="alert alert-success my-3">
+            Email inviata con successo!
+          </div>
+
+          <div class="my-3">
+            <label for="name" class="form-label">Come ti chiami?</label>
+            <input type="name" class="form-control" :class="{'is-invalid':errors.name}" id="name" name="name" v-model="name">
+            <p v-for="(error, index) in errors.name" :key="'error_name'+index" class="invalid-feedback">
+              {{error}}
+            </p>
+          </div>
+
+          <div class="mb-3">
+            <label for="email" class="form-label">Inserisci la tua email</label>
+            <input type="email" class="form-control" :class="{'is-invalid':errors.email}" id="email" name="email" v-model="email">
+            <p v-for="(error, index) in errors.email" :key="'error_email'+index" class="invalid-feedback">
+              {{error}}
+            </p>
+          </div>
+
+          <div class="mb-3">
+            <label for="message" class="form-label">Cosa vuoi dirci</label>
+            <textarea class="form-control" :class="{'is-invalid':errors.message}" id="message" rows="8" name="message" v-model="message"></textarea>
+            <p v-for="(error, index) in errors.message" :key="'error_message'+index" class="invalid-feedback">
+              {{error}}
+            </p>
+          </div>
+
+          <button type="submit" class="btn btn-primary">{{sendingInProgress ? 'Invio in corso..' : 'Invia'}}</button>
+
+        </form>
+      </div>
+    </div>     
   </div>
 </template>
 
 <script>
 export default {
-    name: 'Contact'
-}
+    name: 'Contact',
+
+    data() {
+      return{
+        name: '',
+        email: '',
+        message: '',
+        sendingInProgress: false,
+        errors: {},
+        success: false,
+      }
+    },
+
+    methods: {
+      sendForm() {
+
+        this.sendingInProgress = true;
+        axios.post('/api/contacts', {
+          'name': this.name,
+          'email': this.email,
+          'message': this.message,
+        }).then(response => {
+          this.sendingInProgress = false;
+
+          if (response.data.errors) {
+            this.errors = response.data.errors;
+            this.success = false;
+          }else{
+            this.success = true;
+            this.name = '';
+            this.email = '';
+            this.message = '';
+            this.errors = {};
+          }
+
+          console.log(response);
+        });
+
+      }
+    }
+};
 </script>
 
 <style>
---
+++ resources/views/emails/new-contact.blade.php
@@ -0,0 +1,7 @@
+<h1>Ciao Admin!</h1>
+<p>
+    Hai ricevuto un nuovo contatto dal sito, ecco i dettagli: <br>
+    Nome del contatto: {{$lead->name}}<br>
+    Email del contatto: {{$lead->email}}<br>
+    Messaggio: {{$lead->message}}
+</p>
